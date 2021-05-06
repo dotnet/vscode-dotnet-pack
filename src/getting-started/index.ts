fdfa039b36a806eb607c700b682b625e652f28d7
cartermp <pcarter@fastmail.com>
2021-05-06 14:33:34 -0700

    Switch to .dib, clean outputs

--- src/getting-started/index.ts
+++ src/getting-started/index.ts
@@ -3,58 +3,19 @@
 
 import * as vscode from "vscode";
 import * as path from "path";
-import { loadTextFromFile, getExtensionContext } from "../utils";
-import { sendInfo, instrumentOperation } from "vscode-extension-telemetry-wrapper";
-
-let dotnetGettingStartedView: vscode.WebviewPanel | undefined;
 
 export async function dotnetGettingStartedCmdHandler(context: vscode.ExtensionContext, operationId: string, tabId?: string) {
   const editors = vscode.window.visibleTextEditors;
-  if (editors.some(editor => editor.document.fileName.endsWith('.NET getting started.ipynb'))) {
+  if (editors.some(editor => editor.document.fileName.endsWith('Getting Started with .NET.dib'))) {
     return;
   }
 
-  const notebookPath = context.asAbsolutePath(path.join('misc', '.NET getting started.ipynb'));
+  const notebookPath = context.asAbsolutePath(path.join('misc', 'Getting Started with .NET.dib'));
   const notebookUri = vscode.Uri.file(notebookPath);
 
-  await vscode.commands.executeCommand('vscode.openWith', notebookUri, 'dotnet-interactive-jupyter');
+  await vscode.commands.executeCommand('vscode.openWith', notebookUri, 'dotnet-interactive');
 
   // ignore some values to make webpack happy lol
   operationId;
   tabId;
-}
-
-function onDidDisposeWebviewPanel() {
-  dotnetGettingStartedView = undefined;
-}
-
-async function initializeDotnetGettingStartedView(context: vscode.ExtensionContext, webviewPanel: vscode.WebviewPanel, onDisposeCallback: () => void, operationId: string) {
-  webviewPanel.iconPath = vscode.Uri.file(path.join(context.extensionPath, "logo.lowres.png"));
-  const resourceUri = context.asAbsolutePath("./out/assets/getting-started/index.html");
-  webviewPanel.webview.html = await loadTextFromFile(resourceUri);
-  context.subscriptions.push(webviewPanel.onDidDispose(onDisposeCallback));
-  context.subscriptions.push(webviewPanel.webview.onDidReceiveMessage(async (e) => {
-    if (e.command === "tabActivated") {
-      let tabId = e.tabId;
-      sendInfo(operationId, {
-        infoType: "tabActivated",
-        tabId: tabId
-      });
-    }
-  }));
-}
-
-export class DotnetGettingStartedViewSerializer implements vscode.WebviewPanelSerializer {
-  async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, _state: any) {
-    if (dotnetGettingStartedView) {
-      dotnetGettingStartedView.reveal();
-      webviewPanel.dispose();
-      return;
-    }
-
-    dotnetGettingStartedView = webviewPanel;
-    instrumentOperation("restoreGettingStartedView", operationId => {
-      initializeDotnetGettingStartedView(getExtensionContext(), webviewPanel, onDidDisposeWebviewPanel, operationId);
-    })();
-  }
 }
